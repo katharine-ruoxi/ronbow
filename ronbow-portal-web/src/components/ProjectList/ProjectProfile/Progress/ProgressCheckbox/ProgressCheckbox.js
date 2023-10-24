cb2cb53a93ea2e368ea61ea23add76e2541452ee
Katharine Ruoxi <ruoxi2013@gmail.com>
2023-10-24 01:03:03 -0700

    update

--- ronbow-portal-web/src/components/ProjectList/ProjectProfile/Progress/ProgressCheckbox/ProgressCheckbox.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Checkbox } from 'antd';
-const onChange = (e) => {
-  console.log(`checked = ${e.target.checked}`);
-};
-const App = () => <Checkbox onChange={onChange}></Checkbox>;
-export default App;
